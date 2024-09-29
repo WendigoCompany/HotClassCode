effe70d1b0aa4cd7a39a260300692e9b67dd5c54
Bonafo Bona <cosrikura@gmail.com>
2024-09-28 21:59:52 -0300

    Workin in chat box

--- src/Components/Chat/ChatModal.jsx
+++ src/Components/Chat/ChatModal.jsx
@@ -1,20 +1,87 @@
-export default function ChatModal({setter}) {
+export default function ChatModal({ setter }) {
     return <>
         <div className="modal-bg">
             <div className="modal ch-modal">
                 <div>
-                    <button className="close-btn" onClick={()=>{setter(null)}}>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"  fill="black"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
+                    <button className="close-btn" onClick={() => { setter(null) }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" fill="black"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
                     </button>
                 </div>
-
+                
                 <div className="ch-dial-box">
 
                 </div>
 
-                <div className="ch-list">
-                    
+                <div className="ch-list container">
+
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+                
+                <img  className="item chat-list-g" src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" />
+
+                    {/* <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div>
+                    <div className="item chat-list-g"><img src="https://1drv.ms/i/s!Ak22M1uRqSwpgheuXzBmLnP3hhtA?embed=1" alt="" /></div> */}
+
                 </div>
+
             </div>
         </div>
     </>
