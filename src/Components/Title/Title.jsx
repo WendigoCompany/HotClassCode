a4d22d50a74b98914906be9f35bb6c5636874012
Bonafo Bona <cosrikura@gmail.com>
2024-09-30 00:02:32 -0300

    Fix Title

--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -1,10 +1,17 @@
+import { config } from "../../Router/Routes";
 import "../../Styles/Title/manifiest.css"
 
 export default function Title(){
+    let url = '';
+    if(process.env.REACT_APP_STADE == "gh"){
+        url += config.gh_repo +"#" 
+    }
+    
+
     return <>
 
                 <h1 className={`page-title`} style={{cursor: "pointer"}} onClick={()=>{
-                    window.location.href= "/";
+                    window.location.href= url + "/";
                     
                 }}>   {process.env.REACT_APP_NAME.toUpperCase()}</h1>
       
