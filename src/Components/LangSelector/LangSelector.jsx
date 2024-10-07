79389998fa432bbd3e2870045c4cb3a9e0656a5c
Bonafo Bona <cosrikura@gmail.com>
2024-10-07 19:56:09 -0300

    Finished Router/LangRedirection

--- src/Components/LangSelector/LangSelector.jsx
+++ src/Components/LangSelector/LangSelector.jsx
@@ -1,24 +1,32 @@
 import { useLang } from "../../Context/lang_context";
 import "../../Styles/Lang/manifiest.css";
-export default function LangSelector(){
-    const {lang, setLang} = useLang();
+export default function LangSelector() {
+    const { lang, setLang } = useLang();
     return <>
-    <select value={lang} className="lang-selector" onChange={(e)=>{
-        // setLang(e.target.value)
-        window.location.href = window.location.href.replace(lang, e.target.value)
-        console.log("w",window.location.href);
-        console.log("wlan",e.target.value);
-        console.log("wss",window.location.href.replace(lang, e.target.value));
+        <select value={lang} className="lang-selector" onChange={(e) => {
+            // setLang(e.target.value)
+            // window.location.href = window.location.href.replace(lang, e.target.value)
 
-        if (process.env.REACT_APP_STADE == "gh") {
-            window.location.reload()
-    
-    
-        }
-        w
-    }}>
-        <option value="en">English</option>
-        <option value="es">Español</option>
-    </select>
+
+            if (process.env.REACT_APP_STADE == "gh") {
+
+                const { origin, pathname, hash } = window.location;
+                window.location.href = origin + pathname + hash.replace(`/${lang}`, `/${e.target.value}`);
+                window.location.reload()
+
+
+            } else {
+                const { origin, pathname } = window.location;
+                console.log(window.location);
+                window.location.href = origin + pathname.replace(`/${lang}`, `/${e.target.value}`);
+            }
+
+
+
+
+        }}>
+            <option value="en">English</option>
+            <option value="es">Español</option>
+        </select>
     </>
 }
